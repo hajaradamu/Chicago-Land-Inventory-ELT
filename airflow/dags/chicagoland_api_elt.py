a9d66fbf673bcba4b5911fab2714e84e65450d82
Hajarat J. Adamu <103903080+hajaradamu@users.noreply.github.com>
2023-12-10 19:26:25 +0100

    Update chicagostreet_api_elt.py

--- airflow/dags/chicagoland_api_elt.py
+++ airflow/dags/chicagoland_api_elt.py
@@ -1,9 +1,8 @@
 import os
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.utils.task_group import TaskGroup
 from airflow.operators.empty import EmptyOperator
-from web.operators.plg_api_to_pg_to_gcs import LandInvToPostgresOperator
+from web.operators.chi_api_to_pg_to_gcs import LandInvToPostgresOperator
 from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
@@ -14,12 +13,11 @@ AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-DESTINATION_BUCKET = os.environ.get("GCP_GCS_BUCKET")
+DESTINATION_BUCKET = "bascket1" #os.environ.get("GCP_GCS_BUCKET")
 
-DATASET="chicagolandinv"
+DATASET="raw"
 
-API_TOKEN = os.environ.get("API_TOKEN")
-DATASET = "Chicago_Land_Inv"
+API_TOKEN1 = os.environ.get("API_TOKEN1")
 
 # Database configuration
 PG_HOST = os.environ.get("PG_HOST")
@@ -40,7 +38,7 @@ DEFAULT_ARGS = {
 }
 
 with DAG(
-    dag_id="Load-Land-Data-From-Web-To-PG-To-GCS=To-BQ",
+    dag_id="Load-Land-Data-From-Web-To-PG-To-GCS-To-BQ",
     description="Job to move data from Chicago Land website to Postgres to Google Cloud Storage and then transfer from GCS to BigQuery, and finally create a data model using dbt",
     default_args=DEFAULT_ARGS,
     schedule_interval="0 21 * * *",
@@ -53,7 +51,7 @@ with DAG(
     # Use the new EvictionToGCSBQOperator
     download_web_to_gcs_bq = LandInvToPostgresOperator(
         task_id='download_to_gcs',
-        api_token=API_TOKEN,
+        api_token=API_TOKEN1,
         user=PG_USER,
         password=PG_PASSWORD,
         host=PG_HOST,
@@ -62,21 +60,22 @@ with DAG(
     )
 
     get_data = PostgresToGCSOperator(
-        task_id="transfer_land_inv_data_from_Postgres_to_GCS",
+        task_id="transfer_street_names_data_from_Postgres_to_GCS",
         postgres_conn_id='postgres_default',
-        sql=f"SELECT * FROM chicago_land_data",
+        sql=f"SELECT * FROM chicago_street_names",
         bucket=DESTINATION_BUCKET,
-        filename=f"chicago/land_inv_data.json",
+        filename=f"chicago/street_names_data.json",
         gzip=False,
     )
 
     load_gcs_to_bgquery =  GCSToBigQueryOperator(
         task_id = "load_gcs_to_bgquery",
         bucket=f"{DESTINATION_BUCKET}", #BUCKET
-        source_objects=['land_inv_data.json'], # SOURCE OBJECT
-        destination_project_dataset_table=f"{DATASET}.chicago_land_data", # `nyc.green_dataset_data` i.e table name
+        source_objects=['chicago/street_names_data.json'], # SOURCE OBJECT
+        destination_project_dataset_table=f"{DATASET}.chicago_street_names_data", # `nyc.green_dataset_data` i.e table name
         autodetect=True, #DETECT SCHEMA : the columns and the type of data in each columns of the json file
         write_disposition="WRITE_TRUNCATE", # command to update table from the  latest (or last row) row number upon every job run or task run
+        source_format="NEWLINE_DELIMITED_JSON",
     )
 
     end = EmptyOperator(task_id="end")
